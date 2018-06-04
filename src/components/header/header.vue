6ad57b5f700214df127b780afe2ea1095262e47a
wt <307548594@qq.com>
2018-06-04 20:57:20 +0800

    header内容部分

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,16 +1,163 @@
 <template>
  <div class="header">
-     IamHeader!
+     <div class="content-wrapper">
+         <div class="avatar">
+             <img width="64" height="64" :src="seller.avatar" alt="商家头像">
+         </div>
+         <div class="content">
+             <div class="title">
+                 <span class="brand"></span>
+                 <span class="name">{{seller.name}}</span>
+             </div>
+             <div class="description">
+                 {{seller.description}}/{{seller.deliveryTime}}分钟送达
+             </div>
+             <div v-if="seller.supports" class="support">
+                 <span class="icon" :class="classMap[seller.supports[0].type]"></span>
+                 <span class="text">{{seller.supports[0].description}}</span>
+             </div>
+         </div>
+        <!-- 活动个数 -->
+         <div v-if="seller.supports" class="support-count">
+             <span class="count">{{seller.supports.length}}个</span>
+             <i class="icon-keyboard_arrow_right"></i>
+         </div>
+     </div>
+     <!-- 公告区块 -->
+     <div class="bulletin-wrapper">
+         <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+         <i class="icon-keyboard_arrow_right"></i>
+     </div>
+     <!-- 背景图片 -->
+     <div class="background">
+         <img :src="seller.avatar" width="100%" height="100%" alt="">
+     </div>
  </div>
 </template>
 
 <script>
  export default {
-
+     props: {
+         seller:{
+             type: Object
+         }
+     },
+     created() {
+         this.classMap=['decrease','discount','special','invoice','guarantee'];
+     }
  };
 </script>
 
-<style>
+<style lang="stylus">
+    @import "../../common/stylus/mixin.styl";
 
- 
+    .header
+        position relative
+        color: #ffffff
+        background rgba(7,17,27,0.5)
+        .content-wrapper
+            position relative
+            padding 24px 12px 18px 24px
+            font-size 0 /*消除avatar和content之间的空隙*/
+            .avatar
+                display inline-block
+                vertical-align top
+                img 
+                    border-radius 2px
+            .content
+                display inline-block
+                margin-left 16px
+                // font-size 14px
+                .title
+                    margin 2px 0 8px 0
+                    .brand
+                        display inline-block
+                        vertical-align top
+                        width 30px
+                        height 18px
+                        bg-image('brand')
+                        background-size 30px 18px
+                    .name
+                        margin-left 6px
+                        font-size 16px
+                        line-height 18px
+                        font-weight bold
+                .description
+                    margin-bottom 10px
+                    font-size 12px
+                    line-height 12px
+                .support
+                    .icon
+                        display inline-block
+                        vertical-align top
+                        width 12px
+                        height 12px
+                        margin-right 4px
+                        background-size 12px 12px
+                        background-repeat no-repeat
+                        &.decrease
+                            bg-image('decrease_1')
+                        &.discount
+                            bg-image('discount_1')
+                        &.guarantee
+                            bg-image('guarantee_1')
+                        &.invoice
+                            bg-image('invoice_1')
+                        &.special
+                            bg-image('special_1')
+                    .text
+                        line-height 12px
+                        font-size 10px // chrome最小字体12px
+            .support-count
+                position absolute
+                right 12px
+                bottom 14px
+                padding 0 8px
+                height 24px
+                line-height 24px
+                border-radius 14px
+                background rgba(0,0,0,0.2)
+                text-align center
+                .count
+                    font-size 10px
+                .icon-keyboard_arrow_right
+                    margin-left 2px
+                    line-height 24px
+                    font-size 10px
+        .bulletin-wrapper
+            position relative
+            height 28px
+            line-height 28px
+            // font-size 0  会导致省略号不见
+            padding 0 22px 0 12px
+            white-space nowrap
+            overflow hidden
+            text-overflow ellipsis
+            background rgba(7,17,27,0.2)
+            .bulletin-title
+                display inline-block
+                vertical-align top
+                margin-top 7px
+                width 22px
+                height 12px
+                bg-image('bulletin')
+                background-size 22px 12px
+                background-repeat no-repeat
+            .bulletin-text
+                margin 0 4px 
+                font-size 10px
+            .icon-keyboard_arrow_right
+                position absolute
+                font-size 10px
+                right 12px
+                top 8px
+        .background
+            position absolute
+            top 0px
+            left 0px
+            width 100%
+            height 100%
+            z-index -1
+            filter blur(10px) //图片模糊效果
+
 </style>
