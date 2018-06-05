e9c4713392da2668dcf74e606c18156f163f8af6
wt <307548594@qq.com>
2018-06-05 13:58:37 +0800

    header弹出层 star组件

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -18,13 +18,13 @@
              </div>
          </div>
         <!-- 活动个数 -->
-         <div v-if="seller.supports" class="support-count">
+         <div v-if="seller.supports" class="support-count" @click="showDetail">
              <span class="count">{{seller.supports.length}}个</span>
              <i class="icon-keyboard_arrow_right"></i>
          </div>
      </div>
      <!-- 公告区块 -->
-     <div class="bulletin-wrapper">
+     <div class="bulletin-wrapper" @click="showDetail">
          <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
          <i class="icon-keyboard_arrow_right"></i>
      </div>
@@ -32,18 +32,71 @@
      <div class="background">
          <img :src="seller.avatar" width="100%" height="100%" alt="">
      </div>
+     <!-- 详情弹层 -->
+     <!-- <transition name="fade"> -->
+     <div v-show="detailShow" class="detail"> <!-- transition="fade"-->
+         <div class="detail-wrapper clearfix">
+             <div class="detail-main">
+                 <h1 class="name">{{seller.name}}</h1>
+                 <div class="star-wrapper">
+                     <star :size="48" :score="seller.score"></star>
+                 </div>
+                 <div class="title">
+                     <div class="line"></div>
+                     <div class="text">优惠信息</div>
+                     <div class="line"></div>
+                 </div>
+                 <ul v-if="seller.supports" class="supports"><!-- 几个优惠信息 -->
+                     <li class="support-item" v-for="(item,index) in seller.supports" :key="index">
+                         <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                         <span class="text">{{seller.supports[index].description}}</span>
+                     </li>
+                 </ul>
+                <div class="title">
+                     <div class="line"></div>
+                     <div class="text">商家公告</div>
+                     <div class="line"></div>
+                 </div>
+                 <div class="bulletin">
+                     <p class="content">{{seller.bulletin}}</p>
+                 </div>
+             </div>
+         </div>
+         <div class="detail-close" @click="hideDetail">
+             <i class="icon-close"></i>
+         </div>
+     </div>
+     <!-- </transition> -->
  </div>
 </template>
 
 <script>
+import star from '../star/star.vue';
+
  export default {
      props: {
          seller:{
              type: Object
          }
      },
+     data(){
+         return{
+             detailShow: false
+         }
+     },
+     methods: {
+         showDetail(){
+             this.detailShow = true
+         },
+         hideDetail(){
+             this.detailShow = false
+         }
+     },
      created() {
          this.classMap=['decrease','discount','special','invoice','guarantee'];
+     },
+     components: {
+         star
      }
  };
 </script>
@@ -53,6 +106,7 @@
 
     .header
         position relative
+        overflow hidden
         color: #ffffff
         background rgba(7,17,27,0.5)
         .content-wrapper
@@ -137,7 +191,7 @@
             .bulletin-title
                 display inline-block
                 vertical-align top
-                margin-top 7px
+                margin-top 8px
                 width 22px
                 height 12px
                 bg-image('bulletin')
@@ -159,5 +213,94 @@
             height 100%
             z-index -1
             filter blur(10px) //图片模糊效果
-
+        .detail
+            position fixed
+            z-index 100
+            top 0px
+            left 0px
+            width 100%
+            height 100%
+            overflow auto
+            background rgba(7,17,27,0.8)
+            // backdrop-filter blur(10px)  
+            // &.fade-enter-active,&.fade-leave-active //最终显示的class
+            //     transition all .5s
+            //     opacity 1
+            //     background rgba(7,17,27,0.8)
+            // &.fade-enter,&.fade-leave
+            //     opacity 0
+            //     background rgba(7,17,27,0)
+            .detail-wrapper  //css sticky footer
+                min-height 100%
+                width 100%
+                .detail-main
+                    margin-top 64px
+                    padding-bottom 64px //保证x按钮不遮挡文字
+                    .name
+                        line-height 16px
+                        text-align center
+                        font-size 16px
+                        font-weight 700
+                    .star-wrapper
+                        margin-top 18px
+                        padding 2px 0
+                        text-align center
+                    .title
+                        display flex
+                        width 80%
+                        margin 28px auto 24px auto
+                        .line
+                            flex 1
+                            position relative
+                            top -6px
+                            border-bottom 1px solid rgba(255,255,255,0.2)
+                        .text
+                           padding 0 12px
+                           font-weight 700
+                           font-size 14px
+                    .supports
+                        width 80%
+                        margin 0 auto 
+                        .support-item
+                            padding 0 12px
+                            margin-bottom 12px
+                            font-size 0
+                            &.last-child
+                                margin-bottom 0
+                            .icon
+                                display inline-block
+                                width 16px
+                                height 16px
+                                vertical-align top
+                                margin-right 6px
+                                background-size 16px 16px
+                                background-repeat no-repeat
+                                &.decrease
+                                    bg-image('decrease_2')
+                                &.discount
+                                    bg-image('discount_2')
+                                &.guarantee
+                                    bg-image('guarantee_2')
+                                &.invoice
+                                    bg-image('invoice_2')
+                                &.special
+                                    bg-image('special_2')
+                            .text
+                                line-height 16px
+                                font-size 12px
+                    .bulletin
+                        width 80%
+                        margin 0 auto 
+                        .content
+                            padding 0 12px
+                            line-height 24px
+                            font-size 12px
+            
+            .detail-close
+                position relative
+                width 32px
+                height 32px
+                margin -64px auto 0 auto 
+                clear both
+                font-size 32px
 </style>
