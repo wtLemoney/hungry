c53b697efe632a205b2bb30e99f0e6eb1c548cda
wt <307548594@qq.com>
2018-06-15 17:19:14 +0800

    评论页面

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="ratings">
+    <div class="ratings" ref="ratings">
         <div class="rating-content">
             <div class="overview"><!-- 打分模块 -->
                 <div class="overview-left">
@@ -24,16 +24,66 @@
                     </div>
                 </div>
             </div>
+            <split></split>
+            <ratingselect 
+                    :select-type="selectType" 
+                    :only-content="onlyContent" 
+                    :desc="desc" 
+                    :ratings="ratings"
+                    v-on:ratingtype-select="ratingtypeSelect"
+                    v-on:content-toggle="contentToggle">
+            </ratingselect>
+
+                <div class="rating-wrapper">
+                    <ul>
+                        <li v-show="needShow(rating.rateType,rating.text)" v-for="(rating,index) in ratings" class="rating-item border-1px" :key="index">
+                            <div class="avatar">
+                                <img width="28" height="28" :src="rating.avatar">
+                            </div>
+                            <div class="content">
+                                <h1 class="name">{{rating.username}}</h1>
+                                <div class="star-wrapper">
+                                    <star class="star" :size="24" :score="rating.score"></star>
+                                    <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}</span>
+                                </div>
+                                <p class="text">{{rating.text}}</p>
+                                <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                                    <span class="icon-thumb_up"></span>
+                                    <span class="item" v-for="(item,index) in rating.recommend" :key="index">{{item}}</span>
+                                </div>
+                                <div class="time">
+                                    {{rating.rateTime | formatDate}}
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            
         </div>
     </div>
 </template>
 
 <script>
-import star from '../star/star'
+import star from '../star/star';
+import split from '../split/split';
+import ratingselect from '../ratingselect/ratingselect';
+import {formatDate} from '../../common/js/date.js';
+import BScroll from 'better-scroll';
+
+
+const ALL = 2;
+const ERROR_OK = 0;
 export default {
     data () {
         return {
-
+            ratings: [],
+            selectType: ALL,
+            onlyContent: false,
+            desc: {
+                all: '全部',
+                positive: '满意',
+                negative: '不满意'
+            }
         }
     },
     props: {
@@ -42,12 +92,61 @@ export default {
         }
     },
     components: {
-        star
-    }
-}
+        star,
+        ratingselect,
+        split
+    },
+    created() { //从后端拿到ratings
+        this.$http.get('/api/ratings').then((response)=>{
+            response = response.body;
+            if(response.errno === ERROR_OK) {
+                this.ratings = response.data;
+
+                this.$nextTick(()=>{//保证dom渲染完成再执行
+                    if(!this.scroll){
+                        this.scroll = new BScroll(this.$refs.ratings,{click: true});
+                    }else{
+                        this.scroll.refresh();
+                    }
+                });
+            }
+        });
+    },
+    methods: {
+        needShow(type,text){
+            if(this.onlyContent && !text){
+                return false;
+            }
+            if(this.selectType === ALL){
+                return true;
+            }else{
+                return type === this.selectType;
+            }
+         },
+        ratingtypeSelect(type) {
+            this.selectType = type;
+            this.$nextTick(()=>{//解决better-scroll问题
+                this.scroll.refresh();
+            });
+        },
+        contentToggle(onlyContent) {
+            this.onlyContent = onlyContent;
+            this.$nextTick(()=>{
+                this.scroll.refresh();
+            });
+        }
+    },
+    filters: {
+        formatDate(time) {
+            let date = new Date(time);
+            return formatDate(date,'yyyy-MM-dd hh:mm');//date.js
+        }
+    }, 
+}  
 </script>
 
 <style lang='stylus'>
+@import '../../common/stylus/mixin.styl';
     .ratings
         position fixed
         top 174px
@@ -116,9 +215,73 @@ export default {
                         margin-left 12px
                         font-size 12px
                         color rgb(147,153,159)
+        .rating-wrapper
+            padding 0 18px
+            .rating-item
+                display flex
+                padding 18px 0
+                border-1px(rgba(7,17,27,0.1))
+                .avatar
+                    flex 0 0 28px
+                    width 28px
+                    margin-right 12px
+                    img
+                        border-radius 50%
+                .content
+                    position relative
+                    flex 1
+                    .name
+                        margin-bottom 4px
+                        line-height 12px
+                        font-size 10px
+                        color rgb(7,17,27)
+                    .star-wrapper
+                        margin-bottom 6px
+                        font-size 0
+                        .star
+                            display inline-block
+                            margin-right 6px
+                            vertical-align top
+                        .delivery
+                            display inline-block
+                            vertical-align top
+                            line-height 12px
+                            font-size 10px
+                            color rgb(147,153,159)
+                    .text
+                        margin-bottom 8px
+                        font-size 12px
+                        line-height 18px
+                        color rgb(7,17,27)
+                    .recommend
+                        line-height 16px
+                        font-size 0
+                        .icon-thumb_up,.item
+                            display inline-block
+                            margin 0 8px 4px 0
+                            font-size 9px
+                        .icon-thumb_up
+                            color rgb(0,160,220)
+                        .item
+                            padding 0 6px
+                            border 1px solid rgba(7,17,27,0.1)
+                            border-radius 1px
+                            color rgb(147,153,159)
+                            background #fff
+                    .time
+                        position absolute
+                        top 0
+                        right 0
+                        line-height 12px
+                        font-size 10px
+                        
+
+                        
+
 
 
 
                     
+
 </style>
 
