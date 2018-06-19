e58a8ddee4c3fc2ec3ab415a3b98893b631b8cae
wt <307548594@qq.com>
2018-06-19 15:50:51 +0800

    商家页面 前端存储 keep-alive

--- src/App.vue
+++ src/App.vue
@@ -14,15 +14,18 @@
     </div>
   <!-- 路由出口 -->
   <!-- 路由匹配到的组件将渲染在这里 -->
-  <router-view :seller="seller">
-    <div class="content">
-      I am content
-    </div>
-  </router-view>
+  <keep-alive>  <!-- 缓存所有的页面,将组件状态缓存 -->
+    <router-view :seller="seller">
+      <div class="content">
+        I am content
+      </div>
+    </router-view>
+  </keep-alive>
   </div>
 </template>
 
 <script>
+import {urlParse} from './common/js/util.js'
 import header from './components/header/header.vue';
 
 const ERR_OK = 0;
@@ -30,18 +33,25 @@ const ERR_OK = 0;
 export default {
   data() {
     return {
-      seller:{}
+      seller:{
+        id: (() => {
+          let queryParam = urlParse();
+          // console.log(queryParam.id)
+          return queryParam.id;
+        })()
+      }
     }
   },
   created(){// 钩子函数
   //拿到header部分的数据
-    this.$http.get('/api/seller').then((response) => {
+    this.$http.get('/api/seller?id='+this.seller.id).then((response) => {
       //response = response.json();//.json()返回一个Promise对象
       response = response.body;
       // console.log(response);
       if(response.errno === ERR_OK){
-        this.seller = response.data;
-        console.log(this.seller);     
+        // this.seller = response.data;
+        console.log(this.seller);
+        this.seller = Object.assign({},this.seller,response.data)     
       }
     });
   },
